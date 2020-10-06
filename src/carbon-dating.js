7f8dfd502e5ca1ad37f83d47b892d8bba6367f0d
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-06 05:14:06 +0300

    feat: add carbon-dating task dateSamle function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+
+ var isparameterOk =  (/^\d+$|^\d+\.\d+$/.test(sampleActivity)) //строка состоит из цифр или цифр раделенной еинственнйо точкой
+                   && (typeof(sampleActivity) === 'string')     //передана строка?
+                   && ( Number(sampleActivity) > 0 && Number(sampleActivity) < 15); //число в строке менее 15 и более нуля.
+
+ return  isparameterOk ? Math.ceil(Math.log2(sampleActivity/15) * (-HALF_LIFE_PERIOD)) : false;
+
 };
