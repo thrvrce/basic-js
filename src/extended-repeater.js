9c057062eae590508a4082d53203ac2ed1ab1efe
Victor Avdeev <avdeevvictor95@gmail.com>
2020-10-07 05:30:20 +0300

    feat: add repeater function (extended-repeater task)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options ) {
+ var tmpStr = '';
+
+ if (options.repeatTimes === undefined){
+  options.repeatTimes = 1;
+ }
+ if (options.separator === undefined){
+  options.separator = '+';
+ }
+ if (options.addition === undefined){
+  options.addition = '';
+ }
+ if (options.additionSeparator === undefined){
+  options.additionSeparator = '|';
+ }
+ if (options.addition === undefined){
+  options.addition = '';
+ }
+ if (options.additionRepeatTimes === undefined){
+  options.additionRepeatTimes = 1;
+ }
+
+ for (var i = 1; i <= options.repeatTimes; i++){
+   tmpStr += str;
+   for (var j = 1; j <= options.additionRepeatTimes; j++){
+     tmpStr +=  (j !== options.additionRepeatTimes) ? options.addition + options.additionSeparator : options.addition;
+   }
+   tmpStr += (i !== options.repeatTimes) ? options.separator : '';
+ }
+return tmpStr;
 };
-  
